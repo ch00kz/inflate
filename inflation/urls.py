02db07e6cb6d180439a22df67a84901d4d27c7f7
Andrew Thomas <andr3w.l.thomas@gmail.com>
2015-05-28 19:53:22 -0500

    added index.html and home view function for it

--- inflation/urls.py
+++ inflation/urls.py
@@ -22,6 +22,7 @@ v1_api = Api(api_name='v1')
 v1_api.register(USDExchangeRateResource())
 
 urlpatterns = [
+    url(r'^', 'history.views.home', name="home"),
     url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
